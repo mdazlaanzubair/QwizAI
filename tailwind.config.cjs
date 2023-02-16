05373ff29cac50592ad45426036a214ead275b7f
mdazlaanzubair <mdazlaan1996@gmail.com>
2023-02-16 22:13:43 +0500

    initial commit with app setup

---
+++ tailwind.config.cjs
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+
+  theme: {
+    extend: {},
+  },
+
+  // add daisyUI plugin
+  plugins: [require("daisyui")],
+
+  // config (optional)
+  daisyui: {
+    styled: true,
+    themes: true,
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+  },
+};
